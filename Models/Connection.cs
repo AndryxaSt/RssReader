0ab437967e141f5452ce949b971ce5b08dd3fdd2
Andrey <AndryxaSte@gmail.com>
2020-10-11 20:44:53 +0300

    Added method for removing html tags and checking images

--- Models/Connection.cs
+++ Models/Connection.cs
@@ -6,39 +6,66 @@ using System.Xml;
 using System.IO;
 using System.Net;
 using System.ServiceModel.Syndication;
-
-
+using System.Text.RegularExpressions;
 
 namespace RssReader.Models
 {
     public class Connection
     {
-        public IEnumerable<News> GetRss(string url)
+        private string RemoveHtmlTags(string inputHtml)
         {
+            inputHtml = inputHtml.Replace("<![CDATA[", "").Replace("]]>", "");
+
+            inputHtml = Regex.Replace(inputHtml, "<.*?>", string.Empty).Replace("&nbsp;", string.Empty);
+
+            return inputHtml;
+        }
+        public IEnumerable<IEnumerable<News>> GetRss(IEnumerable<Subscription> subscriptions)
+        {
+            List<List<News>> outputSubs = new List<List<News>>();
             List<News> news;
 
-            XmlReader FeedReader = XmlReader.Create(url);
-            SyndicationFeed Channel = SyndicationFeed.Load(FeedReader);
-
-            if (Channel != null)
+            if (subscriptions != null)
             {
-                news = new List<News>();
-
-                foreach (SyndicationItem RSI in Channel.Items)
+                foreach (var subscription in subscriptions)
                 {
-                    news.Add(new News()
-                    {
-                        Title = RSI.Title.Text,
-                        ImgSrc = RSI.Links[1].Uri.AbsoluteUri,
-                        Discription = RSI.Summary.Text,
-                        Url = RSI.Id,
-                        PubDate = RSI.PublishDate.DateTime
-                    });
+                    news = new List<News>();
+                    XmlReader FeedReader = XmlReader.Create(subscription.Url);
+                    SyndicationFeed Channel = SyndicationFeed.Load(FeedReader);
 
+                    if (Channel != null)
+                    {
+                        int indexImg = -1;
+
+
+                        for (int i = 0; i < Channel.Items.First().Links.Count; i++)
+                        {
+                            if (Channel.Items.First().Links[i].MediaType == "image/jpeg")
+                            {
+                                indexImg = i;
+                            }
+                        }
+
+
+                        foreach (SyndicationItem RSI in Channel.Items)
+                        {
+                            news.Add(new News()
+                            {
+                                SubsTitle = Channel.Title.Text,
+                                Title = RSI.Title.Text,
+                                ImgSrc = indexImg == -1 ? "Заглушка" : RSI.Links[indexImg].Uri.AbsoluteUri,
+                                Discription = RemoveHtmlTags(RSI.Summary.Text),
+                                Url = RSI.Id,
+                                PubDate = RSI.PublishDate.DateTime
+                            });
+                        }
+
+                        outputSubs.Add(news);
+                    }
                 }
-                return news;
+                return outputSubs;
             }
-            return new List<News>();
+            return outputSubs;
         }
     }
 }
